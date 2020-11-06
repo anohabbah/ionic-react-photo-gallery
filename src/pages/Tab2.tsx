02a53c37d6db86c659ef0dc58dbddadb5d0bc551
Abbah <anohabbah@gmail.com>
2020-11-06 03:26:51 +0100

    Add photo page

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import { camera } from 'ionicons/icons';
+
 import './Tab2.css';
+
+const takePhoto = () => {};
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+      <IonContent>
+          <IonFab vertical="bottom" horizontal="center" slot="fixed">
+              <IonFabButton onClick={() => takePhoto()}>
+                  <IonIcon icon={camera} />
+              </IonFabButton>
+          </IonFab>
       </IonContent>
     </IonPage>
   );
