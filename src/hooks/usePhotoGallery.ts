c949d58958c4c49f9aec9117993b0ebb96e153a8
Abbah <anohabbah@gmail.com>
2020-11-06 12:29:46 +0100

    Loading Photos from the Filesystem

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -4,13 +4,30 @@ import { useFilesystem, base64FromPath } from '@ionic/react-hooks/filesystem';
 import { useStorage } from '@ionic/react-hooks/storage';
 import { isPlatform } from '@ionic/react';
 import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
-import {save} from "ionicons/icons";
 
 const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+const { get, set } = useStorage();
+const PHOTO_STORAGE = "photos";
 
 export function usePhotoGallery() {
     const {getPhoto} = useCamera();
     const [photos, setPhotos] = useState<Photo[]>([]);
+
+    useEffect(() => {
+        const loadSaved = async () => {
+            const photosString = await get(PHOTO_STORAGE);
+            const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+            for (let photo of photos) {
+                const file = await readFile({
+                    path: photo.filepath,
+                    directory: FilesystemDirectory.Data
+                });
+                photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+            }
+            setPhotos(photos);
+        };
+        loadSaved();
+    }, []);
 
     const takePhoto = async () => {
         const cameraPhoto: CameraPhoto = await getPhoto({
@@ -23,6 +40,8 @@ export function usePhotoGallery() {
         const savedImage: Photo = await savePicture(cameraPhoto, filename);
         const newPhotos: Photo[] = [savedImage, ...photos];
         setPhotos(newPhotos);
+
+        set(PHOTO_STORAGE, JSON.stringify(newPhotos));
     };
 
     return {takePhoto, photos};
